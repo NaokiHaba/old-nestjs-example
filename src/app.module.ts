459a01b3c20a3c77d459d898542aed4096dd2bff
naoki-haba <naosprintrunner6385@gmail.com>
2022-12-25 21:54:59 +0900

    chore: typeormとの接続確率

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    AppModule,
+    // 環境変数から取得することを推奨します
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'test',
+      password: 'password',
+      logging: true,
+      database:'test',
+      entities: ['dist/**/entities/**/*.entity.js'],
+      migrations: ['dist/**/migrations/**/*.js'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
