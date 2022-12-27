ddb2545c6044703a25418e84c94008a3462e69ac
NaokiHaba <naosprintrunner6385@gmail.com>
2022-12-27 16:10:34 +0900

    feat: create処理のサービス実装

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  /**
+   *
+   * @param name userName
+   * @param email userEmailAddress
+   */
+  async create({ name, email }: CreateUserDto): Promise<User> {
+    return await this.userRepository
+      .save({
+        name: name,
+        email: email,
+      })
+      .catch((e) => {
+        throw new InternalServerErrorException(
+          `[${e.message}]：アカウントの登録に失敗しました。`,
+        );
+      });
   }
 
   findAll() {
