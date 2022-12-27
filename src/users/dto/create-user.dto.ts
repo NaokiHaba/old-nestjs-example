f9ca4cd89c826891e584174ee4fd4b89be6e99be
NaokiHaba <naosprintrunner6385@gmail.com>
2022-12-27 16:02:35 +0900

    feat: update dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @MaxLength(255)
+  @IsNotEmpty()
+  name: string;
+
+  @MaxLength(255)
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+}
